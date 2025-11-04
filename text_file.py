b0981d919764e9442fc733963d7eedcd40decb1e
Natalie Siliezar <nata@DezmansMac.local>
2025-11-04 15:43:49 -0500

    Added edit number 1

--- text_file.py
+++ text_file.py
@@ -1,2 +1,6 @@
 #Practice adding, commit, and push to remote repository.
 #NOTES 2
+
+#Edit file, add changes, commit, and pushing them to the remote repository.
+
+    #1 Edit, change, commit, and push.
